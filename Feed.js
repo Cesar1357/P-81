71e3fcafd49b25d6a57e9957e359080562f86211
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2022-02-22 17:05:41 -0600

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+export default class createPost extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Feed</Text>
+            </View>
+        )
+    }
+}
